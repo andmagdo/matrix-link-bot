df08476bb3403a47aeafb3c9fa8cb314607d335d
andmagdo <62532857+andmagdo@users.noreply.github.com>
2021-05-16 17:33:25 -0500

    Update link_detector.py
    Add functionality to use headers as a MIME check - not tested if websites may spoof this

--- api/link_detector.py
+++ api/link_detector.py
@@ -1,12 +1,13 @@
 from urllib.parse import urlparse
 from os.path import splitext
 import magic
+import requests
 
 mimes = ["video", "image"]  # Note, only checks five characters! - for now, hardcoded, maybe put in a config?
 mime = magic.Magic(mime=True)
 
 
-def getType(url):  # Used to find the file and extention
+def getType(url):  # Used to find the file and extension
     parsed = urlparse(url)
     root, ext = splitext(parsed.path)
     root = root.replace('/', '')  # remove slashes
@@ -15,10 +16,20 @@ def getType(url):  # Used to find the file and extention
 
 
 def checkType(path):  # Uses magic to get a base mime type, then checks it
-    typemime = mime.from_file(path)  #get mime
+    typemime = mime.from_file(path)  # get mime
+    return checkMIME(typemime)
+
+
+def checkContent(url):
+    r = requests.get(url, stream=True)
+    contentType = r.headers['Content-Type'].split(';')[0]
+    return checkMIME(contentType)
+
+
+def checkMIME(mimeType):
     for x in mimes:
-        if typemime[0:5].lower() == x[0:5]:  
-            return typemime  # Whatever typemine is, it will be truthy
+        if mimeType[0:5].lower() == x[0:5]:
+            return mimeType  # Whatever mimeType is, it will be truthy
     return False
 
 
